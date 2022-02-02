3540fe0b605706a11d86812d1013cfc16a11186c
jackiestchen <jackiestchen@gmail.com>
2022-02-01 19:59:56 -0700

    Fix syntax error

--- db/seeds/02_items.sql
+++ db/seeds/02_items.sql
@@ -2,22 +2,22 @@ INSERT INTO items (
   name, description, price, image_url
 ) VALUES (
   'Classic', 'Signature bubble tea with 3 toppings: bubbles, pudding and grass jelly', 6.99, 'https://images.squarespace-cdn.com/content/v1/5edfb8e7559cd9129deae815/1629474098758-PX9OU33P0MIPXHMDD4JL/2+Guys.png'
-)
+);
 
 INSERT INTO items (
   name, description, price, image_url
-) VALUES INTO (
+) VALUES (
   'Strawberry Latte',  'Fresh milk with strawberry and boba', 6.99, 'https://coco-tea.ph/wp-content/uploads/2021/04/Strawberry-Au-Lait.png'
-)
+);
 
 INSERT INTO items (
   name, description, price, image_url
-) VALUES INTO (
+) VALUES (
   'Mango Slush',  'Fresh milk blended with ice and mango for a perfect balance of slushy and chewy bobab', 6.99, 'https://images.squarespace-cdn.com/content/v1/5edfb8e7559cd9129deae815/1607618450914-QVSBI4WFA7BQHHVJORD6/Mango+Slush'
-)
+);
 
 INSERT INTO items (
   name, description, price, image_url
-) VALUES INTO (
+) VALUES (
   'Amazon Water',  'Fresh water from the Andes', 6.99, 'https://www.tibetanbuddhistaltar.org/wp-content/uploads/2011/02/iStock_000004511195Medium.jpg'
-)
+);
