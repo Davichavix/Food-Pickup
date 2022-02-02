9a21bad3cd66c78598582ceb6dcd1aea64275b4a
jackiestchen <jackiestchen@gmail.com>
2022-02-01 21:57:58 -0700

    Add items

--- db/seeds/02_items.sql
+++ db/seeds/02_items.sql
@@ -15,6 +15,26 @@ INSERT INTO items (
 ) VALUES (
   'Mango Slush',  'Fresh milk blended with ice and mango for a perfect balance of slushy and chewy bobab', 6.99, 'https://images.squarespace-cdn.com/content/v1/5edfb8e7559cd9129deae815/1607618450914-QVSBI4WFA7BQHHVJORD6/Mango+Slush'
 );
+INSERT INTO items (
+  name, description, price, image_url
+) VALUES (
+  'Black Sugar Milk w/ Cream Mouse',  'This drink is formulated with precision. Our sugar and ice level are perfectly balanced to deliver the most delicious drink to you.', 7.99, 'https://s3-media0.fl.yelpcdn.com/bphoto/4jZEfsUv0LuWdBXr_RHZuA/l.jpg'
+);
+INSERT INTO items (
+  name, description, price, image_url
+) VALUES (
+  'Black Sugar Lemon Oolong Tea',  'Black sugar infused boba blended with lemon juice and oolong tea', 6.49, 'https://scontent.fyyc3-1.fna.fbcdn.net/v/t1.6435-9/116214595_3172861439450398_914265421773839750_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=9267fe&_nc_ohc=uIz4oiC738oAX92CBwO&_nc_ht=scontent.fyyc3-1.fna&oh=00_AT_oDILve4b-rxlPfvi-pSy4kW1vzdHNeTSHDnEZFfQPdA&oe=621DBF52'
+);
+INSERT INTO items (
+  name, description, price, image_url
+) VALUES (
+  'Green Tea Latte Boba',  'Fresh milk mixed with perfectly brewed green tea and boba', 6.99, 'https://data.thefeedfeed.com/static/2019/06/05/15597629795cf81823d037c.jpg'
+);
+INSERT INTO items (
+  name, description, price, image_url
+) VALUES (
+  'Cheese Mousse Boba Pearl Milk',  'Whipped cheese mosse stirred with milk tea and boba', 6.99, 'https://cdn.vox-cdn.com/thumbor/VQTx2oXZRWTYMGtN-K-c4OHUBPo=/0x0:4223x3167/1200x800/filters:focal(1902x1200:2576x1874)/cdn.vox-cdn.com/uploads/chorus_image/image/61481407/cheesetea_shutterstock.0.jpg'
+);
 
 INSERT INTO items (
   name, description, price, image_url
