ade53d45d28ffd4e8ba2e65eb76254ef6cd200b6
David Chan <dchan902@gmail.com>
2022-02-01 19:05:50 -0700

    add Home_page route

--- server.js
+++ server.js
@@ -37,6 +37,7 @@ app.use(express.static("public"));
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
 const widgetsRoutes = require("./routes/widgets");
+const homePageRoutes = require("./routes/home_page");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
