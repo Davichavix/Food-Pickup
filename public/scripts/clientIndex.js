681f83c2826161b8714c37b155a1c58ebabe5db7
David Chan <dchan902@gmail.com>
2022-02-04 20:46:57 -0700

    complete localstorage of item to cart

---
+++ public/scripts/clientIndex.js
@@ -0,0 +1,17 @@
+// Client facing scripts here
+$(document).ready(function() {
+  const addtoCartBtn = document.getElementsByClassName('item')
+  let items = [];
+  for (let i = 0; i < addtoCartBtn.length; i++) {
+    addtoCartBtn[i].addEventListener('click', function(e) {
+      let item = {
+        "item-image_url": e.target.parentElement.children[0].getAttribute('src'),
+        "item-name": e.target.parentElement.children[1].children[0].textContent,
+        "item-description": e.target.parentElement.children[1].children[1].textContent,
+        "item-price": e.target.parentElement.children[2].textContent
+      }
+      items.push(item);
+      localStorage.setItem('items', JSON.stringify(items));
+    })
+  }
+})
