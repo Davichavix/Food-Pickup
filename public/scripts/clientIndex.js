c9152726f39359c8a0c59e6fcd0399a9230cb4a4
David Chan <dchan902@gmail.com>
2022-02-04 23:48:18 -0700

    modify localstorage to include item id

--- public/scripts/clientIndex.js
+++ public/scripts/clientIndex.js
@@ -1,20 +1,22 @@
 // Client facing scripts here
 $(document).ready(function() {
-  let items = [];
+  let items = {};
   if(localStorage.getItem('items') !== null) {
     items = JSON.parse(localStorage.getItem('items'))
   };
   const addtoCartBtn = document.getElementsByClassName('add-button')
   for (let i = 0; i < addtoCartBtn.length; i++) {
     addtoCartBtn[i].addEventListener('click', function(e) {
+      let itemId = e.target.parentElement.getAttribute('id');
       let item = {
+        "id": itemId,
         "image_url": e.target.parentElement.children[0].getAttribute('src'),
         "name": e.target.parentElement.children[1].children[0].textContent,
         "description": e.target.parentElement.children[1].children[1].textContent,
         "price": e.target.parentElement.children[2].textContent,
         "qty": 1
       }
-      items.push(item);
+      items[itemId] = item;
       localStorage.setItem('items', JSON.stringify(items));
       $('.badge').text(JSON.parse(localStorage.getItem('items')).length);
     })
