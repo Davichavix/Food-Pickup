ff7ea0a8863b3bbfda14b1e960f7b5aa19918bea
isissi <isissi_525@hotmail.com>
2022-02-05 23:01:38 +0000

    fix table code & split into three tables

--- public/scripts/user_orders_id.js
+++ public/scripts/user_orders_id.js
@@ -1,4 +1,5 @@
 $(document).ready(() => {
+  // loadTitle();
   loadOrders();
 
 });
@@ -10,47 +11,51 @@ $(document).ready(() => {
 
   const creatOrder = (orderData) => {
     const html = `
-    <tbody>
-      <tr>
-          <td>Order status:</td>
-          <td>${orderData.status}</td>
-      </tr>
-      <tr>
-        <td>Name:</td>
-        <td>${orderData.user_name}</td>
-      </tr>
-      <tr>
-          <td>Phone Number:</td>
-          <td>${orderData.phone_number}</td>
-      </tr>
-      <tr>
-        <td>Email:</td>
-        <td>${orderData.email}</td>
-      </tr>
-      <tr>
-        <td>Placed at:</td>
-        <td>${orderData.created_at}</td>
-      </tr>
-      <tr>
-        <td>Pickup at:</td>
-        <td>${orderData.ready_at}</td>
-      </tr>
-      <tr>
-        <td>Order details:</td>
-          <table class="order-details">
-            <tr>
-              <td>Bubble Tea</td>
-              <td>x 2</td>
-            </tr>
-            <tr>
-              <td>Mango Slush</td>
-              <td>  x 1</td>
-            </tr>
-        </table>
-      </tr>
+    <table>
+      <tbody>
+        <tr>
+            <td>Order status:</td>
+            <td>${orderData.status}</td>
+        </tr>
+        <tr>
+          <td>Name:</td>
+          <td>${orderData.user_name}</td>
+        </tr>
+        <tr>
+            <td>Phone Number:</td>
+            <td>${orderData.phone_number}</td>
+        </tr>
+        <tr>
+          <td>Email:</td>
+          <td>${orderData.email}</td>
+        </tr>
+        <tr>
+          <td>Placed at:</td>
+          <td>${orderData.created_at}</td>
+        </tr>
+        <tr>
+          <td>Pickup at:</td>
+          <td>${orderData.ready_at}</td>
+        </tr>
+
+      </tbody>
+    </table>
+    `
+
+    return html;
+  }
+
+  const createOrderItems = () => {
+    const html = `
+      <table class='items'>
+        <tr>
+          Order items:
+        </tr>
+      </table>
+
       <hr class="checkout-divider">
-      <div class="checkout-price">
-        <table>
+
+      <table>
         <tr>
           <td>Subtotal:</td>
           <td>$100</td>
@@ -63,32 +68,37 @@ $(document).ready(() => {
           <td>Total:</td>
           <td>$105</td>
         </tr>
-        </table>
-      </div>
-    </tbody>
-    `
+      </table>
+    `;
 
     return html;
   }
 
 
-  const renderOrderDetail = (order) => {
+  const renderOrder = (order) => {
 
-    const newOrder = creatOrder(order['orders']);
-    const container = $('.table-contesnt');
-    container.prepend(newOrder);
+    const newOrder = creatOrder(order['order'][0]);
+    //console.log(newOrder);
+    const container = $('.checkout-table');
+    container.append(newOrder);
+  }
 
+  const renderItems = () => {
+    const orderItems = createOrderItems();
+    const container = $('.checkout-table');
+    container.append(orderItems);
   }
 
   const loadOrders = () => {
     // console.log(window.location.pathname);
     const path =window.location.pathname;
-    const user_id = path.split('/')[2];
-    // const order_id = path.split('/')[3];
+    // const user_id = path.split('/')[2];
+    const order_id = path.split('/')[3];
 
-    $.get(`/api/orders/user/${user_id}`).then((res) => {
-      console.log(res);
-      renderOrders(res);
+    $.get(`/api/orders/details/${order_id}`).then((res) => {
+      // console.log(res);
+      renderOrder(res);
+      renderItems();
     });
 
     // $.$get(`/api/orders/user/`)
