2c0dca0b333eb8ef7e574f95ae9c2c907002c72c
jackiestchen <jackiestchen@gmail.com>
2022-02-06 22:57:32 -0700

    Clean up code

--- public/scripts/checkout-complete.js
+++ public/scripts/checkout-complete.js
@@ -1,10 +1,7 @@
 $(() => {
   const userId = 1; //Add cookie session
 
-  window.history.pushState(null, "", window.location.href);
-  window.onpopstate = () => {
-    window.history.pushState(null, "", window.location.href);
-  }
+
 
   setTimeout(() => {
     window.location.href = `/orders/${userId}`;
