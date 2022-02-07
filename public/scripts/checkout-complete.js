5e720e2085a9c089cf28ea296eec615369c43cd3
jackiestchen <jackiestchen@gmail.com>
2022-02-06 22:54:26 -0700

    Change titles

--- public/scripts/checkout-complete.js
+++ public/scripts/checkout-complete.js
@@ -1,7 +1,14 @@
 $(() => {
   const userId = 1; //Add cookie session
 
+  window.history.pushState(null, "", window.location.href);
+  window.onpopstate = () => {
+    window.history.pushState(null, "", window.location.href);
+  }
+
   setTimeout(() => {
     window.location.href = `/orders/${userId}`;
   }, 2000);
+
+
 });
