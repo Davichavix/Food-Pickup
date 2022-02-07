5393909a0ffd40a0e268ce3899c641e64369f87a
jackiestchen <jackiestchen@gmail.com>
2022-02-07 00:04:01 -0700

    Change password to encrypted

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,21 +1,21 @@
 INSERT INTO users (
   first_name, last_name, phone_number, email, password, isOwner
 ) VALUES (
-  'David', 'Chan', '123456789', '123@gmail.com', '12345', false
+  'David', 'Chan', '123456789', '123@gmail.com', '$2b$12$IQr3Qmt5UMafYGYI3IK9kuomVpvNBiIuvqNa3Cvnh5k0to1gxitcK', false
 );
 INSERT INTO users (
   first_name, last_name, phone_number, email, password, isOwner)
   VALUES (
-  'Siyi', 'Xie', '987654321', '456@gmail.com', '12345', false
+  'Siyi', 'Xie', '987654321', '456@gmail.com', '$2b$12$IQr3Qmt5UMafYGYI3IK9kuomVpvNBiIuvqNa3Cvnh5k0to1gxitcK', false
 );
 INSERT INTO users (
   first_name, last_name, phone_number, email, password, isOwner
 ) VALUES (
-  'Jacky', 'Tchen', '567894321', '789@gmail.com', '12345', false
+  'Jacky', 'Tchen', '567894321', '789@gmail.com', '$2b$12$IQr3Qmt5UMafYGYI3IK9kuomVpvNBiIuvqNa3Cvnh5k0to1gxitcK', false
 );
 INSERT INTO users (
   first_name, last_name, phone_number, email, password, isOwner
 ) VALUES (
-  'Coco', 'Admin', '1112223333', 'admin@coco.com', '12345', true
+  'BTS', 'Admin', '1112223333', 'admin@bbtshop.com', '$2b$12$IQr3Qmt5UMafYGYI3IK9kuomVpvNBiIuvqNa3Cvnh5k0to1gxitcK', true
 );
 
