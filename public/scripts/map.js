04223c0fe03f5808a3710fffafb3ecda1a4112fb
isissi <isissi_525@hotmail.com>
2022-02-08 20:34:16 +0000

    add footer to all pages

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -9,7 +9,7 @@ function initMap() {
   const uluru = { lat: 49.281290, lng: -123.114670 };
   // The map, centered at Uluru
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 13,
+    zoom: 14,
     center: uluru,
   });
   // The marker, positioned at Uluru
